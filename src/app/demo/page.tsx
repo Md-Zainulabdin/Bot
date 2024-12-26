4c3e69655c3a16ff11b6979e2fb28ecb3d7e29ca
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2024-12-26 12:59:00 +0500

    feat: add demo page

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Demo() {
+  return (
+    <div className="grid h-screen w-full place-items-center">
+      <h1 className="text-3xl font-bold tracking-tighter">Demo</h1>
+    </div>
+  );
+}
+
+export default Demo;
